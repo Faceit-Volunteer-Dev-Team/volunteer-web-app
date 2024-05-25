018ec38dde49172d341227d7ab72d00c303eccc8
Flo <stefanescuf@ymail.com>
2024-05-25 11:20:40 +0300

    favicon edited

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Homepage() {
   return (
     <>
       <title>Volunteer Application Form</title>
+      <link rel="icon" href="/favicon.ico" />
       <div className="overflow-hidden pb-[16.4rem] md:pb-[25.6rem]">
         <Container className="pt-[6.4rem]">
           <HomepageHero />
