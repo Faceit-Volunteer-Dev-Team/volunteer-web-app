da659859cd011d3adeeab7d1bc36fd0905398ab0
Flo <stefanescuf@ymail.com>
2024-05-25 12:49:52 +0300

    removed unused animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,6 @@ module.exports = {
       "3xl": "2.6rem",
       "4xl": "3.2rem",
       "5xl": "4rem",
-      "6xl": ["4.4rem", "1.1"],
-      "7xl": ["4.8rem", "1.1"],
-      "8xl": ["8rem", "1.1"],
     },
     colors: {
       transparent: "transparent",
@@ -58,16 +55,6 @@ module.exports = {
         "linear-gradient(92.88deg, #757575 9.16%, #B0C4DE 43.89%, #708090 64.72%)",
       "page-gradient":
         "radial-gradient(ellipse 80% 50% at 50% -20%,rgba(112, 128, 144, 0.3), transparent)",
-      "hero-gradient":
-        "radial-gradient(ellipse 50% 80% at 20% 40%,rgba(112, 128, 144, 0.1),transparent), radial-gradient(ellipse 50% 80% at 80% 50%,rgba(119, 136, 153, 0.15),transparent)",
-      "hero-glow":
-        "conic-gradient(from 230.29deg at 51.63% 52.16%, #757575 0deg, #B0C4DE 67.5deg, #708090 198.75deg, #757575 251.25deg, #708090 301.88deg, #B0C4DE 360deg)",
-      "glow-lines":
-        "linear-gradient(var(--direction), #757575 0.43%, #708090 14.11%, rgba(112, 128, 144, 0) 62.95%)",
-      "radial-faded":
-        "radial-gradient(circle at bottom center, #757575, transparent 70%)",
-      "glass-gradient":
-        "linear-gradient(rgba(112, 128, 144, 0) 0%, rgba(112, 128, 144, 0.05) 100%)",
     },
 
     transitionDelay: {
@@ -78,68 +65,9 @@ module.exports = {
         from: { opacity: 0, transform: "translateY(-10px)" },
         to: { opacity: 1, transform: "none" },
       },
-      "image-rotate": {
-        "0%": { transform: "rotateX(25deg)" },
-        "25%": { transform: "rotateX(25deg) scale(0.9)" },
-        "60%": { transform: "none" },
-        "100%": { transform: "none" },
-      },
-      "image-glow": {
-        "0%": {
-          opacity: 0,
-          "animation-timing-function": "cubic-bezier(0.74,0.25,0.76,1)",
-        },
-        "10%": {
-          opacity: 1,
-          "animation-timing-function": "cubic-bezier(0.12,0.01,0.08,0.99)",
-        },
-        "100%": {
-          opacity: 0.2,
-        },
-      },
-      "sketch-lines": {
-        "0%": { "stroke-dashoffset": 1 },
-        "50%": { "stroke-dashoffset": 0 },
-        "99%": { "stroke-dashoffset": 0 },
-        "100%": { visiblity: "hidden" },
-      },
-      "glow-line-horizontal": {
-        "0%": { opacity: 0, transform: "translateX(0)" },
-        "5%": { opacity: 1, transform: "translateX(0)" },
-        "90%": { opacity: 1 },
-        "100%": { opacity: 0, transform: "translateX(min(60vw, 45rem))" },
-      },
-      "glow-line-vertical": {
-        "0%": { opacity: 0, transform: "translateY(0)" },
-        "5%": { opacity: 1, transform: "translateY(0)" },
-        "90%": { opacity: 1 },
-        "100%": { opacity: 0, transform: "translateY(min(21vw, 45rem))" },
-      },
-      zap: {
-        "0%, 9%, 11%, 100% ": {
-          fill: "transparent",
-        },
-        "10%": {
-          fill: "white",
-        },
-      },
-      bounce: {
-        "50%": {
-          transform: "scale(0.98)",
-        },
-      },
     },
     animation: {
       "fade-in": "fade-in 1000ms var(--animation-delay, 0ms) ease forwards",
-      "image-rotate": "image-rotate 1400ms ease forwards",
-      "image-glow": "image-glow 4100ms 600ms ease-out forwards",
-      "sketch-lines": "sketch-lines 1200ms ease-out forwards",
-      "glow-line-horizontal":
-        "glow-line-horizontal var(--animation-duration) ease-in forwards",
-      "glow-line-vertical":
-        "glow-line-vertical var(--animation-duration) ease-in forwards",
-      zap: "zap 2250ms calc(var(--index) * 20ms) linear infinite",
-      bounce: "240ms ease 0s 1 running bounce",
     },
   },
   plugins: [],
