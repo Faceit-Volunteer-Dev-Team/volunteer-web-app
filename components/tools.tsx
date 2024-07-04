973aa22851ae0957ea09e90f7e1bdecca040cda3
shadiflo <92731974+shadiflo@users.noreply.github.com>
2024-07-04 14:20:48 +0200

    match moderation preview

---
+++ components/tools.tsx
@@ -0,0 +1,16 @@
+export const Tools = () => (
+  <div className="text-white  font-Play flex items-center justify-center min-h-[48rem] ">
+    <div className="relative order-last content-center flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-start     p-8 text-center md:max-w-[calc(80.66%-12px)] md:basis-[calc(80.66%-12px)] md:p-14">
+      <img
+        src="/admin6.png"
+        alt="Admin Background"
+        className="absolute inset-0 h-full w-full object-contain"
+      />
+      <img
+        src="/match.png"
+        alt="Match"
+        className="absolute mx-auto my-auto h-auto  w-1/3 inset-0"
+      />
+    </div>
+  </div>
+);
